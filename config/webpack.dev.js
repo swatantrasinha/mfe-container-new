4e1f6acda0d9f4773f0833f443fe6cc1b018ede1
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2021-07-01 21:29:27 +0530

    ok

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -8,7 +8,7 @@ const packageJson= require('../package.json');
 const devConfig = {
     mode: 'development',
     devServer: {
-        port: 8080,
+        port: 8085,
         historyApiFallback: {
             index: 'index.html',
         },
@@ -17,7 +17,7 @@ const devConfig = {
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: 'marketing@https://mfe-marketing.herokuapp.com/dist/remoteEntry.js',
+                marketing: 'marketing@http://localhost:8081/remoteEntry.js',
             },
             shared : packageJson.dependencies,
         }),
