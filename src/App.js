ff23fbc71a8ced54326b8abe437c89881046ac88
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2021-07-02 23:06:01 +0530

    adding header changs in container will face css collision in prod

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import MarketingApp  from './components/MarketingApp';
 import {mount} from 'marketing/MarketingApp';
+import Header from './components/Header';
+import {BrowserRouter} from 'react-router-dom';
 
 console.log('mount in container app : ', mount);
 
 
 export default () => {
     return (<div>
-                <h1> This is container App!!!</h1>
-                <hr/>
-                <MarketingApp />
+                {/* <h1> This is container App!!!</h1> */}
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <MarketingApp />
+                    </div>
+                </BrowserRouter>
             </div>
             );
 };
