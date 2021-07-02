245803829afd9763c4b483e3c097fb7f6b88cd3a
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2021-07-02 23:28:11 +0530

    fixing css conflicts

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import MarketingApp  from './components/MarketingApp';
 import {mount} from 'marketing/MarketingApp';
 import Header from './components/Header';
 import {BrowserRouter} from 'react-router-dom';
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
 console.log('mount in container app : ', mount);
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 
 export default () => {
     return (<div>
-                {/* <h1> This is container App!!!</h1> */}
                 <BrowserRouter>
-                    <div>
-                        <Header />
-                        <MarketingApp />
-                    </div>
+                    <StylesProvider generateClassName={generateClassName}>
+                        <div>
+                            <Header />
+                            <MarketingApp />
+                        </div>
+                    </StylesProvider>
                 </BrowserRouter>
             </div>
             );
